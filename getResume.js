d1dd21d41822abc3a5028cd5aa8a0b8386d430b8
VERCHERE Rémi <remi.verchere2@gmail.com>
2025-03-16 22:19:00 +0100

    ajout des options de configuration

--- getResume.js
+++ getResume.js
@@ -3,60 +3,55 @@
 const btn = document.getElementById("resumeLog")
 const spinner = document.getElementById("spinner")
 const inputFile = document.getElementById("logFile")
-const geminiKeyInput = document.getElementById("geminiKey")
 
 
 btn.addEventListener("click", () => {
-  
-  const GOOGLE_API_KEY = geminiKeyInput.value;
 
-  
-  const fReader = new FileReader();
-  fReader.readAsText(inputFile.files[0]);
-  fReader.onloadend = function(event){
+  chrome.storage.local.get(["my_gemini_log_key"]).then((result) => {
+
+    const GOOGLE_API_KEY = result.my_gemini_log_key;
+
+    const fReader = new FileReader();
+    fReader.readAsText(inputFile.files[0]);
+    fReader.onloadend = function(event){
     
-    const logsText = event.target.result;
-    //console.log("val",logsLines)
+        const logsText = event.target.result;
+        //console.log("val",logsLines)
 
-    const lines = logsText.split("\n");
+        const lines = logsText.split("\n");
 
-    var errorLines =lines.filter(l => {
-        return l.includes("ERROR")
-    })
+        var errorLines =lines.filter(l => {
+            return l.includes("ERROR")
+        })
 
-    const payload = {
-        "contents": [{
-        "parts":[{"text": `fais une synthèse des lignes de logs suivantes: ${errorLines}` }]
-        }]
-    };
+        const payload = {
+            "contents": [{
+            "parts":[{"text": `fais une synthèse des lignes de logs suivantes: ${errorLines}` }]
+            }]
+        };
     
-    const url = "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?" + new URLSearchParams({
-        key: GOOGLE_API_KEY
-    }).toString();
+        const url = "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?" + new URLSearchParams({
+            key: GOOGLE_API_KEY
+        }).toString();
     
-    spinner.hidden = false;
+        spinner.hidden = false;
 
-    fetch(url, {
-        method: 'POST',
-        
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(payload),
-    }).then(response => response.json())
-    .then(data => {
-        // console.log(data);
-        const analyse = data.candidates[0].content.parts[0].text;
+        fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(payload),
+        }).then(response => response.json())
+        .then(data => {
+            // console.log(data);
+            const analyse = data.candidates[0].content.parts[0].text;
 
-        document.getElementById("analyse").innerText = analyse;
-        spinner.hidden = true;
+            document.getElementById("analyse").innerText = analyse;
+            spinner.hidden = true;
 
-    })
-
-
-}
-
-  
-
-})
+        });
+    }
+  });
+});
 
