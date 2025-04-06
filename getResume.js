46cf9ac078dc280f36db73656117dcd2c71f1103
VERCHERE Rémi <remi.verchere2@gmail.com>
2025-04-06 13:55:44 +0200

    ajout check forms

--- getResume.js
+++ getResume.js
@@ -1,4 +1,16 @@
-
+function parseLogLine(line) {
+    const regexDef = /^([0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2},[0-9]{3})\s+([A-Z]+)\s+(.*)$/;
+    const match = line.match(regexDef);
+    if (match) {
+        return {
+            date: match[1],
+            level: match[2],
+            message: match[3]
+        };
+    } else {
+        return null;
+    }
+}
 
 const btn = document.getElementById("resumeLog")
 const spinner = document.getElementById("spinner")
@@ -55,112 +67,114 @@ function getSystemPrompt(mode) {
                
 }
 
-function parseLogLine(line) {
-    const regexDef = /^([0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2},[0-9]{3})\s+([A-Z]+)\s+(.*)$/;
-    const match = line.match(regexDef);
-    if (match) {
-        return {
-            date: match[1],
-            level: match[2],
-            message: match[3]
-        };
-    } else {
-        return null;
-    }
-}
+
 
 btn.addEventListener("click", () => {
 
-  chrome.storage.local.get(["my_gemini_log_key"]).then((result) => {
-
-    const GOOGLE_API_KEY = result.my_gemini_log_key;
-
-    const fReader = new FileReader();
-    console.log(document.getElementsByName("logFile"))
-    fReader.readAsText(document.getElementsByName("logFile")[0].files[0]);
-    fReader.onloadend = function(event){
-    
-        const logsText = event.target.result;
-
-        const lines = logsText.split("\n");
-        
-        let formattedLines = [];
-        for (let index = 0; index < lines.length; index++) {
-            const matchLine = parseLogLine(lines[index]);
-
-            if (matchLine) {
-                formattedLines.push(`${matchLine.date} ${matchLine.level} ${matchLine.message}`);
-            } else {
-                formattedLines[formattedLines.length - 1] = formattedLines[formattedLines.length - 1] + "\n" + lines[index];
-            }
-        }
-
-        const keyword = getKeyword();
-
-        var errorLines =formattedLines.filter(l => {
-            return l.includes(keyword)
-        })
-        if (errorLines.length === 0) {
-            document.getElementById("analyse").innerHTML = "<p>Aucune ligne de log trouvée avec ces mots clés<p>";
-            return;
-        }
-
-        //console.log("errorLines", errorLines)
-        //console.log("errorLines", errorLines[0])
-        //console.log("errorLines", errorLines[1])
-
-        const systemInstruction = getSystemPrompt(formTagElements["mode"].value);
-
-        const payload = {
-            "system_instruction":{
-                "parts":[
-                    {
-                        "text": systemInstruction
-                    }
-                ]
-            },
-            "contents": {
-                "parts":[
-                    {
-                        "text": `fais une synthèse des lignes de logs suivantes: ${errorLines}` 
-                    }
-                ]
-            }
-        };
-        
-
- 
-        const url = "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?" + new URLSearchParams({
-            key: GOOGLE_API_KEY
-        }).toString();
-    
-        spinner.hidden = false;
-
-        fetch(url, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json; charset=UTF-8',
-            },
-            body: JSON.stringify(payload),
-        }).then(response => {
-            if (!response.ok) {
-                throw new Error(`HTTP error! status: ${response.status}`);
-            }
-            return response.json()
-        })
-        .then(data => {
-            const analyse = data.candidates[0].content.parts[0].text;
-            console.log(analyse);
-
-            document.getElementById("analyse").innerHTML = cleanGeminiHtml(analyse);
-            spinner.hidden = true;
-
-        }).catch((error) => {
-            console.error('Error:', error.message);
-            alert("Erreur lors de l'analyse du log");
-            spinner.hidden = true;
-        });
+    //form check
+    if (document.getElementsByName("logFile").length === 0 || document.getElementsByName("logFile")[0].files.length === 0) {
+        alert("Veuillez sélectionner un fichier de log à analyser.");
+        return;
     }
-  });
+
+    if (document.getElementsByName("mode").length === 0 || formTagElements["mode"].value === "") {
+        alert("Veuillez sélectionner un mode d'analyse.");
+        return;
+    }
+
+    if (formTagElements["mode"].value === "custom" && document.getElementsByName("keywordSearch").length > 0 && document.getElementsByName("keywordSearch")[0].value === "") {
+        alert("Veuillez entrer un mot clé pour l'analyse personnalisée.");
+        return;
+    }
+    chrome.storage.local.get(["my_gemini_log_key"]).then((result) => {
+
+        const GOOGLE_API_KEY = result.my_gemini_log_key;
+
+        const fReader = new FileReader();
+        console.log(document.getElementsByName("logFile"))
+        fReader.readAsText(document.getElementsByName("logFile")[0].files[0]);
+        fReader.onloadend = function(event){
+        
+            const logsText = event.target.result;
+
+            const lines = logsText.split("\n");
+            
+            let formattedLines = [];
+            for (let index = 0; index < lines.length; index++) {
+                const matchLine = parseLogLine(lines[index]);
+
+                if (matchLine) {
+                    formattedLines.push(`${matchLine.date} ${matchLine.level} ${matchLine.message}`);
+                } else {
+                    formattedLines[formattedLines.length - 1] = formattedLines[formattedLines.length - 1] + "\n" + lines[index];
+                }
+            }
+
+            const keyword = getKeyword();
+
+            var errorLines =formattedLines.filter(l => {
+                return l.includes(keyword)
+            })
+            if (errorLines.length === 0) {
+                document.getElementById("analyse").innerHTML = "<p>Aucune ligne de log trouvée avec ces mots clés<p>";
+                return;
+            }
+
+            //console.log("errorLines", errorLines)
+            //console.log("errorLines", errorLines[0])
+            //console.log("errorLines", errorLines[1])
+
+            const systemInstruction = getSystemPrompt(formTagElements["mode"].value);
+
+            const payload = {
+                "system_instruction":{
+                    "parts":[
+                        {
+                            "text": systemInstruction
+                        }
+                    ]
+                },
+                "contents": {
+                    "parts":[
+                        {
+                            "text": `fais une synthèse des lignes de logs suivantes: ${errorLines}` 
+                        }
+                    ]
+                }
+            };
+            
+
+    
+            const url = "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?" + new URLSearchParams({
+                key: GOOGLE_API_KEY
+            }).toString();
+        
+            spinner.hidden = false;
+
+            fetch(url, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json; charset=UTF-8',
+                },
+                body: JSON.stringify(payload),
+            }).then(response => {
+                if (!response.ok) {
+                    throw new Error(`HTTP error! status: ${response.status}`);
+                }
+                return response.json()
+            })
+            .then(data => {
+                const analyse = data.candidates[0].content.parts[0].text;
+
+                document.getElementById("analyse").innerHTML = cleanGeminiHtml(analyse);
+                spinner.hidden = true;
+
+            }).catch((error) => {
+                console.error('Error:', error.message);
+                alert("Erreur lors de l'analyse du log");
+                spinner.hidden = true;
+            });
+        }
+    });
 });
 
