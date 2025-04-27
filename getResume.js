971fcf8f328149dd0437ddc06a017dfdeb127d87
VERCHERE Rémi <remi.verchere2@gmail.com>
2025-04-27 14:45:05 +0200

    ajout format log custom

--- getResume.js
+++ getResume.js
@@ -1,6 +1,4 @@
-import {getSystemPrompt, parseLogLine, getKeyword} from "./lib/utils.js"; 
-
-
+import {getSystemPrompt, parseLogLine, getKeyword, cleanGeminiHtml} from "./lib/utils.js"; 
 
 const btn = document.getElementById("resumeLog")
 const spinner = document.getElementById("spinner")
@@ -9,7 +7,7 @@ const formTagElements = document.getElementById("formAnalysis").elements;
 for (let index = 0; index < formTagElements["mode"].length; index++) {
     const radioBtn = formTagElements["mode"][index];
     radioBtn.addEventListener("change", () => {
-        console.log(formTagElements["mode"].value)
+
         if (document.getElementsByName("keywordSearch").length > 0) {
             const inputKeyword = document.getElementsByName("keywordSearch")[0];
             if (formTagElements["mode"].value === "custom") {
@@ -21,11 +19,19 @@ for (let index = 0; index < formTagElements["mode"].length; index++) {
     })
 }
 
-function cleanGeminiHtml(html) {
-    const parser = new DOMParser();
-    const doc = parser.parseFromString(html, 'text/html');
-    const div = doc.querySelector('.geminiAnswer');
-    return div ? div.innerHTML : html;
+for (let index = 0; index < formTagElements["format"].length; index++) {
+    const radioBtn = formTagElements["format"][index];
+    radioBtn.addEventListener("change", () => {
+
+        if (document.getElementsByName("regexCustom").length > 0) {
+            const inputRegex = document.getElementsByName("regexCustom")[0];
+            if (formTagElements["format"].value === "custom") {
+                inputRegex.hidden = false;
+            } else {
+                inputRegex.hidden = true;
+            }
+        }
+    })
 }
 
 
@@ -46,6 +52,11 @@ btn.addEventListener("click", () => {
         alert("Veuillez entrer un mot clé pour l'analyse personnalisée.");
         return;
     }
+
+    if (formTagElements["format"].value === "custom" && document.getElementsByName("regexCustom").length > 0 && document.getElementsByName("regexCustom")[0].value === "") {
+        alert("Veuillez renseigner la regex de votre ligne de log.");
+        return;
+    }
     chrome.storage.local.get(["my_gemini_log_key"]).then((result) => {
 
         const GOOGLE_API_KEY = result.my_gemini_log_key;
@@ -60,8 +71,14 @@ btn.addEventListener("click", () => {
             const lines = logsText.split("\n");
             
             let formattedLines = [];
+            let regexDef = /^([0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2},[0-9]{3})\s+([A-Z]+)\s+(.*)$/;
+            if (formTagElements["format"].value === "custom") {
+                regexDef = new RegExp(document.getElementsByName("regexCustom")[0].value);
+
+            }
+            
             for (let index = 0; index < lines.length; index++) {
-                const matchLine = parseLogLine(lines[index]);
+                const matchLine = parseLogLine(lines[index],regexDef);
 
                 if (matchLine) {
                     formattedLines.push(`${matchLine.date} ${matchLine.level} ${matchLine.message}`);
@@ -80,9 +97,6 @@ btn.addEventListener("click", () => {
                 return;
             }
 
-            //console.log("errorLines", errorLines)
-            //console.log("errorLines", errorLines[0])
-            //console.log("errorLines", errorLines[1])
 
             const systemInstruction = getSystemPrompt(formTagElements["mode"].value);
 
@@ -126,7 +140,7 @@ btn.addEventListener("click", () => {
             .then(data => {
                 const analyse = data.candidates[0].content.parts[0].text;
 
-                document.getElementById("analyse").innerHTML = cleanGeminiHtml(analyse);
+                document.getElementById("analyse").innerHTML = cleanGeminiHtml(analyse,'.geminiAnswer');
                 spinner.hidden = true;
 
             }).catch((error) => {
